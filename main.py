155a0ef37e7360fdf043f1dafb788c1b2819564a
James <james.k.hamilton@outlook.com>
2025-09-13 12:47:45 +0100

    error handling

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from project.inverterFunctions import controlMain, chargeTimes, chargeNow
 from project.wallboxStatus import checkStatus
 
 def control() -> None:
-    status = checkStatus()
-    print(status)
+    try:
+        status = checkStatus()
+    except Exception as e:
+        print(f"Failed to get status: {e}")
+        asyncio.run(controlMain(0, chargeTimes)) #set to default charge times on fail
 
     if status == 'CHARGING':
         asyncio.run(controlMain(1, None)) #update the time
         asyncio.run(controlMain(0, chargeNow))
-    else:    
+    else:
         asyncio.run(controlMain(0, chargeTimes))
 
     return None
